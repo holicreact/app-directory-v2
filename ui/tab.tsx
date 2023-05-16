a1b47cf4682304c06644519cc389a59b04ea7cd3
holicreact <holicreact@plingcast.com>
2023-05-16 15:22:22 +0900

    test: prefetch={false}

--- ui/tab.tsx
+++ ui/tab.tsx
@@ -18,6 +18,7 @@ export const Tab = ({ path, item }: { path: string; item: Item }) => {
   return (
     <Link
       href={href}
+      prefetch={false}
       className={clsx('rounded-lg px-3 py-1 text-sm font-medium', {
         'bg-gray-700 text-gray-100 hover:bg-gray-500 hover:text-white':
           !isActive,
